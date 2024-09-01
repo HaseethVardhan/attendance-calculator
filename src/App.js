43b73e3744cb967d6a12cc0e025fa6001c6330f0
HaseethVardhan <haseethvardhan01@gmail.com>
2024-09-01 10:18:08 +0530

    Added State to Form Inputs

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 import Navbar from "./components/Navbar.js"
 import Form from "./components/Form.js"
+import React from "react"
 
 function App() {
+
+  const [attdt, setAttdt] = React.useState({
+    totalclass: '',
+    totalclassatt: '',
+    atttill: ''
+  })
+
+
+  function handleChange(event){
+    const {name, value} = event.target;
+    setAttdt((prevState) => {
+      return {
+        ...prevState,
+        [name]: value
+      }
+    })
+  }
+
+  console.log(attdt);
+
   return (
     <div>
       <Navbar />
-      <Form />
+      <Form attdt={attdt} handleChange={handleChange}/>
     </div>
   );
 }
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,17 +1,17 @@
-export default function Form(){
+export default function Form(props){
     return(
         <form>
             <div className="form-elem">
                 <label htmlFor="totalclass">Total Number of Classes</label>
-                <input type="number" id="totalclass" name="totalclass"></input>
+                <input type="number" id="totalclass" name="totalclass" onChange={props.handleChange} value={props.attdt.totalclass}></input>
             </div>
             <div className="form-elem">
                 <label htmlFor="totalclassatt">Total Number of Classes Attended</label>
-                <input type="number" id="totalclassatt" name="totalclassatt"></input>
+                <input type="number" id="totalclassatt" name="totalclassatt" onChange={props.handleChange} value={props.attdt.totalclassatt}></input>
             </div>
             <div className="form-elem">
                 <label htmlFor="atttill">Attendance till</label>
-                <input type="date" id="atttill" name="atttill"></input>
+                <input type="date" id="atttill" name="atttill" onChange={props.handleChange} value={props.attdt.atttill}></input>
             </div>
             <div className="form-elem">
                 <button>Submit</button>
